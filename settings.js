37534b7f024f2f341463ee4ed5a5fa2ddfbdaba6
Камиль Айзатуллин <46605572+gimbarrkiller@users.noreply.github.com>
2023-12-26 14:00:45 +0300

    add files1

--- settings.js
+++ settings.js
@@ -1,14 +1,15 @@
 window.settings = {
-  cardRegistrationPage: false,
-  handleGlobalKeydownEnter: true,
-  noSizeIframe: true,
+  cardRegistrationPage: false, //Если включина тогда это страница для регистрации карты
+  handleGlobalKeydownEnter: true, //Если включина тогда можно проходить формы через enter
+  noSizeIframe: true, //Игнорируются размеры iframe присланные PGA
   isToggleButtonEnabled: false,
   openApi: {
     baseUrl: "https://lt.pga.gazprombank.ru/api",
     version: "v4",
     portal: "5BAE2814BB16ED0AA4A146AA4A4E168D",
   },
-  baseUrl: "",
+  baseUrl: "pages-mmc", // настройка указывает роутеру location в конфиге nginx
+  //redirectTimeout: 10, // Автоматический редирект по backUrl на странице результатов
   localization: {
     defaultLang: "ru",
     allowedLanguages: ["ru", "en"],
@@ -20,12 +21,22 @@ window.settings = {
   },
   requestCancellation: [
     {
-      timeout: 10000,
+      timeout: 100000,
       matchUrls: ["/3ds2-prepare/accept"],
     },
   ],
   qrCodeSize: 182, //default 256
+  mirPayDesktopShow: false,
   googlePay: {
-    gpay: false,
+    // googlePayEnvironment: 'PRODUCTION', // default is 'TEST'
+    //googlePayRequestType: 'DIRECT', // default is 'PAYMENT_GATEWAY'. DIRECT используется до версии PGA 4.2.10
+    // googlePayAllowedCardNetworks: ['AMEX', 'DISCOVER', 'INTERAC', 'JCB', 'MASTERCARD', 'VISA'],
+    // googlePayAllowedCardAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+    gpay: false, //отключает google pay
   },
+  stateInProgress: "no",
+  applePay: {
+    applePayEnabled: false, // отключает--включает applePay
+  },
+  mirPayResetTimer: 5, // При mirPay перезапрос статуса через mirPayResetTimer секунд на проверку успешного платежа.
 };
